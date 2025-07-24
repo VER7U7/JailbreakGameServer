9cd520105dc3c580ba010da0be4746c2409b40a9
VER7U7 <telegram: @ver7u7>
2025-07-24 17:22:12 +0300

    Initial commit

---
+++ src/main/java/com/VER7U7/Main.java
@@ -0,0 +1,11 @@
+package com.VER7U7;
+
+import com.VER7U7.UnityPhysics.JUPP.JUPPMain;
+
+public class Main {
+    public static void main(String[] args) {
+        JUPPMain juppMain = new JUPPMain((short) 6767);
+
+        while(juppMain.bridgeWork) {}
+    }
+}
